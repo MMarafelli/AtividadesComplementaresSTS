ed7609e4ed78f3190a6068a9735054ae0c34838b
Matheus Marafelli <mmarafelli@outlook.com>
2018-11-29 23:00:35 -0200

    Correção do delete cascade de atividades em domain

--- src/main/java/br/edu/iftm/atividadeComplementar/domains/Atividade.java
+++ src/main/java/br/edu/iftm/atividadeComplementar/domains/Atividade.java
@@ -1,11 +1,15 @@
 package br.edu.iftm.atividadeComplementar.domains;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.NotNull;
 
 @Entity
@@ -27,6 +31,9 @@ public class Atividade implements Serializable {
 	
 	@NotNull(message="Campo percentualPorAtividade precisa ser preenchido")
 	private Integer percentualPorAtividade;
+	
+	@OneToMany(mappedBy="codigo", cascade=CascadeType.ALL)
+	private List<LancamentoAtividade> lancamentoAtividade = new ArrayList<>();
 
 	public Atividade() {
 	}
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -8,3 +8,9 @@ values ('Monitoria', 150, 1, 50),
 ('Integrante de Núcleo ou Grupo de Estudo', 100, 3, 20),
 ('Cursos e mini-cursos', 20, 2, 40);
 
+INSERT INTO LANCAMENTO_ATIVIDADE ( ALUNO_RA,  ATIVIDADE_CODIGO, DATA_INICIO, DATA_FIM, QUANTIDADE_HORAS)
+VALUES ( 11113,  1,  '2018-11-18',  '2018-12-18', 50), 
+( 11113,  3,  '2018-06-18',  '2018-12-18', 50), 
+( 11113,  3,  '2018-05-18',  '2018-10-18', 50), 
+( 59320492, 4,  '2018-03-18',  '2018-06-18', 50), 
+( 59320492,  5,  '2018-01-18',  '2018-09-18', 50);
